0e9c54cc36052f19368df50142c075e4a015f756
ravishankar <ravishankar2106@gmail.com>
2015-11-16 23:43:35 +0530

    Updated sql

--- CommunityApp/sql/master-schema.sql
+++ CommunityApp/sql/master-schema.sql
@@ -17,13 +17,15 @@ CREATE TABLE IF NOT EXISTS `address_template` (
   PRIMARY KEY (`ADDRESS_TEMPLATE_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+INSERT INTO `address_template` (`ADDRESS_TEMPLATE_ID`, `ADDRESS_TEMPLATE_NAME`) VALUES
+	(1, 'APARTMENT-BLOCK');
 
 
 -- Dumping structure for table smart_nehiborhood.address_template_group
 CREATE TABLE IF NOT EXISTS `address_template_group` (
   `ADDRESS_TEMPLATE_GROUP_ID` int(10) NOT NULL,
   `ADDRESS_TEMPLATE_ID` int(10) NOT NULL,
+  `ADDRESS_TEMPLATE_ORDER` int(3) NOT NULL,
   `ADDRESS_TEMPLATE_TYPE_ID` int(3) NOT NULL,
   `ADDRESS_TYPE_NAME` varchar(50) NOT NULL,
   `ADDRESS_TEMPLATE_PARENT_TYPE_ID` int(3) NOT NULL,
@@ -34,7 +36,15 @@ CREATE TABLE IF NOT EXISTS `address_template_group` (
   CONSTRAINT `fk_ADDRESS_TEMPLATE_ADDRESS_TEMPLATE1` FOREIGN KEY (`ADDRESS_TEMPLATE_ID`) REFERENCES `address_template` (`ADDRESS_TEMPLATE_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+INSERT INTO `address_template_group` (`ADDRESS_TEMPLATE_GROUP_ID`, `ADDRESS_TEMPLATE_ID`, `ADDRESS_TEMPLATE_ORDER`, `ADDRESS_TEMPLATE_TYPE_ID`, `ADDRESS_TYPE_NAME`, `ADDRESS_TEMPLATE_PARENT_TYPE_ID`, `COMMON_ADDRESS_FLAG`, `POSTAL_ADDRESS_FLAG`) VALUES
+	(1, 1, 1, 1, 'BLOCK', 0, 0, 1),
+	(2, 1, 2, 2, 'FLOOR', 1, 0, 0),
+	(3, 1, 3, 3, 'FLAT NO', 2, 0, 1),
+	(4, 1, 1, 4, 'ADDRESS1', 0, 1, 1),
+	(5, 1, 2, 5, 'CITY', 0, 1, 1),
+	(6, 1, 3, 6, 'STATE', 0, 1, 1),
+	(7, 1, 4, 7, 'COUNTRY', 0, 1, 1),
+	(8, 1, 5, 8, 'ZIPCODE', 0, 1, 0);
 
 
 -- Dumping structure for table smart_nehiborhood.community_admin_group
@@ -48,8 +58,6 @@ CREATE TABLE IF NOT EXISTS `community_admin_group` (
   CONSTRAINT `fk_COMMUNITY_ADMIN_GROUP_COMMUNITY1` FOREIGN KEY (`COMMUNITY_ID`) REFERENCES `community_master` (`COMMUNITY_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-
 
 -- Dumping structure for table smart_nehiborhood.community_license_type
 CREATE TABLE IF NOT EXISTS `community_license_type` (
@@ -58,17 +66,15 @@ CREATE TABLE IF NOT EXISTS `community_license_type` (
   PRIMARY KEY (`COMMUNITY_LICENSE_TYPE_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+INSERT INTO `community_license_type` (`COMMUNITY_LICENSE_TYPE_ID`, `COMMUNITY_LICENSE_TYPE_NAME`) VALUES
+	(1, 'PAID'),
+	(2, 'TRAIL'),
+	(3, 'DEMO');
 
 
 -- Dumping structure for table smart_nehiborhood.community_master
 CREATE TABLE IF NOT EXISTS `community_master` (
   `COMMUNITY_ID` int(10) NOT NULL,
-  `COMMUNITY_ADDRESS_MAIN` varchar(255) DEFAULT NULL,
-  `COMMUNITY_ZIP_CODE` varchar(10) DEFAULT NULL,
-  `COMMUNITY_CITY` varchar(50) NOT NULL,
-  `COMMUNITY_STATE` varchar(50) NOT NULL,
-  `COMMUNITY_COUNTRY` varchar(50) NOT NULL,
   `COMMUNITY_NAME` varchar(50) NOT NULL,
   `COMMUNITY_NICK_NAME` varchar(50) NOT NULL,
   `COMMUNITY_URL` varchar(255) DEFAULT NULL,
@@ -90,8 +96,6 @@ CREATE TABLE IF NOT EXISTS `community_master` (
   CONSTRAINT `fk_COMMUNITY_MASTER_COMMUNITY_TYPE1` FOREIGN KEY (`COMMUNITY_TYPE_ID`) REFERENCES `community_type` (`COMMUNITY_TYPE_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-
 
 -- Dumping structure for table smart_nehiborhood.community_status
 CREATE TABLE IF NOT EXISTS `community_status` (
@@ -100,8 +104,10 @@ CREATE TABLE IF NOT EXISTS `community_status` (
   PRIMARY KEY (`COMMUNITY_STATUS_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-
+INSERT INTO `community_status` (`COMMUNITY_STATUS_ID`, `COMMUNITY_STATUS_NAME`) VALUES
+	(1, 'ACTIVE'),
+	(2, 'INACTIVE'),
+	(3, 'ONHOLD');
 
 -- Dumping structure for table smart_nehiborhood.community_type
 CREATE TABLE IF NOT EXISTS `community_type` (
@@ -110,7 +116,10 @@ CREATE TABLE IF NOT EXISTS `community_type` (
   PRIMARY KEY (`COMMUNITY_TYPE_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+INSERT INTO `community_type` (`COMMUNITY_TYPE_ID`, `COMMUNITY_TYPE_NAME`) VALUES
+	(1, 'APARTMENT'),
+	(2, 'VILLA'),
+	(3, 'LAYOUT');
 
 
 -- Dumping structure for table smart_nehiborhood.community_vendor_details
@@ -127,20 +136,17 @@ CREATE TABLE IF NOT EXISTS `community_vendor_details` (
   CONSTRAINT `fk_COMMUNITY_VENDOR_DETAILS_COMMUNITY_VENDOR_TYPE1` FOREIGN KEY (`COMMUNITY_VENDOR_TYPE_ID`) REFERENCES `community_vendor_type` (`COMMUNITY_VENDOR_TYPE_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
 
-
--- Dumping structure for table smart_nehiborhood.community_vendor_type
-CREATE TABLE IF NOT EXISTS `community_vendor_type` (
-  `COMMUNITY_VENDOR_TYPE_ID` int(10) NOT NULL,
+-- Dumping structure for table smart_nehiborhood.community_vendor_mapping
+CREATE TABLE IF NOT EXISTS `community_vendor_mapping` (
   `COMMUNITY_ID` int(10) NOT NULL,
-  `COMMUNITY_VENDOR_TYPE_NAME` varchar(20) NOT NULL,
-  PRIMARY KEY (`COMMUNITY_VENDOR_TYPE_ID`),
-  KEY `fk_COMMUNITY_VENDOR_TYPE_COMMUNITY_MASTER1` (`COMMUNITY_ID`),
-  CONSTRAINT `fk_COMMUNITY_VENDOR_TYPE_COMMUNITY_MASTER1` FOREIGN KEY (`COMMUNITY_ID`) REFERENCES `community_master` (`COMMUNITY_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `VENDOR_TYPE_ID` int(10) NOT NULL,
+  PRIMARY KEY (`COMMUNITY_ID`,`VENDOR_TYPE_ID`),
+  KEY `fk_COMMUNITY_VENDOR_MAPPING_VENDOR_TYPE1` (`VENDOR_TYPE_ID`),
+  CONSTRAINT `fk_COMMUNITY_VENDOR_MAPPING_RESIDENT_MASTER1` FOREIGN KEY (`COMMUNITY_ID`) REFERENCES `resident_master` (`RESIDENT_MASTER_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_COMMUNITY_VENDOR_MAPPING_VENDOR_TYPE1` FOREIGN KEY (`VENDOR_TYPE_ID`) REFERENCES `vendor_type` (`VENDOR_TYPE_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
 
 
 -- Dumping structure for table smart_nehiborhood.contact_query
@@ -155,8 +161,6 @@ CREATE TABLE IF NOT EXISTS `contact_query` (
   KEY `fk_CONTACT_QUERY_QUERY_STATUS1` (`QUERY_STATUS_ID`),
   CONSTRAINT `fk_CONTACT_QUERY_QUERY_STATUS1` FOREIGN KEY (`QUERY_STATUS_ID`) REFERENCES `query_status` (`QUERY_STATUS_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Data exporting was unselected.
 
 
 -- Dumping structure for table smart_nehiborhood.personal_information
@@ -174,17 +178,12 @@ CREATE TABLE IF NOT EXISTS `personal_information` (
   KEY `fk_PHONE_NO` (`PHONE_NO`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-
-
 -- Dumping structure for table smart_nehiborhood.query_status
 CREATE TABLE IF NOT EXISTS `query_status` (
   `QUERY_STATUS_ID` int(3) NOT NULL,
   `QUERY_STATUS_NAME` varchar(20) NOT NULL,
   PRIMARY KEY (`QUERY_STATUS_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Data exporting was unselected.
 
 
 -- Dumping structure for table smart_nehiborhood.relationship_type
@@ -194,7 +193,13 @@ CREATE TABLE IF NOT EXISTS `relationship_type` (
   PRIMARY KEY (`RELATIONSHIP_TYPE_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+INSERT INTO `relationship_type` (`RELATIONSHIP_TYPE_ID`, `RELATIONSHIP_TYPE_NAME`) VALUES
+	(1, 'FATHER'),
+	(2, 'MOTHER'),
+	(3, 'SPOUCE'),
+	(4, 'CHILD'),
+	(5, 'FRIEND'),
+	(6, 'OTHERS');
 
 
 -- Dumping structure for table smart_nehiborhood.residence_unit
@@ -211,8 +216,6 @@ CREATE TABLE IF NOT EXISTS `residence_unit` (
   CONSTRAINT `fk_RESIDENCE_UNIT_COMMUNITY_MASTER1` FOREIGN KEY (`COMMUNITY_ID`) REFERENCES `community_master` (`COMMUNITY_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-
 
 -- Dumping structure for table smart_nehiborhood.resident_custom_field
 CREATE TABLE IF NOT EXISTS `resident_custom_field` (
@@ -225,8 +228,6 @@ CREATE TABLE IF NOT EXISTS `resident_custom_field` (
   KEY `fk_RESIDENT_CUSTOM_FIELD_COMMUNITY1` (`COMMUNITY_ID`),
   CONSTRAINT `fk_RESIDENT_CUSTOM_FIELD_COMMUNITY1` FOREIGN KEY (`COMMUNITY_ID`) REFERENCES `community_master` (`COMMUNITY_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Data exporting was unselected.
 
 
 -- Dumping structure for table smart_nehiborhood.resident_custom_field_values
@@ -242,8 +243,6 @@ CREATE TABLE IF NOT EXISTS `resident_custom_field_values` (
   CONSTRAINT `fk_RESIDENT_CUSTOM_FIELD_VALUES_RESIDENT_MASTER1` FOREIGN KEY (`RESIDENT_MASTER_ID`) REFERENCES `resident_master` (`RESIDENT_MASTER_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-
 
 -- Dumping structure for table smart_nehiborhood.resident_location
 CREATE TABLE IF NOT EXISTS `resident_location` (
@@ -257,8 +256,6 @@ CREATE TABLE IF NOT EXISTS `resident_location` (
   KEY `fk_RESIDENT_LOCATION_RESIDENT_MASTER1` (`RESIDENT_MASTER_ID`),
   CONSTRAINT `fk_RESIDENT_LOCATION_RESIDENT_MASTER1` FOREIGN KEY (`RESIDENT_MASTER_ID`) REFERENCES `resident_master` (`RESIDENT_MASTER_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Data exporting was unselected.
 
 
 -- Dumping structure for table smart_nehiborhood.resident_master
@@ -283,8 +280,6 @@ CREATE TABLE IF NOT EXISTS `resident_master` (
   CONSTRAINT `fk_RESIDENT_MASTER_RESIDENT_STATUS1` FOREIGN KEY (`RESIDENT_STATUS_ID`) REFERENCES `resident_status` (`RESIDENT_STATUS_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-
 
 -- Dumping structure for table smart_nehiborhood.resident_relation_mapping
 CREATE TABLE IF NOT EXISTS `resident_relation_mapping` (
@@ -299,8 +294,6 @@ CREATE TABLE IF NOT EXISTS `resident_relation_mapping` (
   CONSTRAINT `fk_RESIDENT_RELATION_MAPPING_RELATIONSHIP_TYPE1` FOREIGN KEY (`RELATIONSHIP_TYPE_ID`) REFERENCES `relationship_type` (`RELATIONSHIP_TYPE_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-
 
 -- Dumping structure for table smart_nehiborhood.resident_status
 CREATE TABLE IF NOT EXISTS `resident_status` (
@@ -309,9 +302,11 @@ CREATE TABLE IF NOT EXISTS `resident_status` (
   PRIMARY KEY (`RESIDENT_STATUS_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-
-
+INSERT INTO `resident_status` (`RESIDENT_STATUS_ID`, `RESIDENT_STATUS_NAME`) VALUES
+	(1, 'ACTIVE'),
+	(2, 'ACTIVATION_PENDING'),
+	(3, 'INACTIVE'),
+	(4, 'DEACTIVATED');
 -- Dumping structure for table smart_nehiborhood.super_admin_group_users
 CREATE TABLE IF NOT EXISTS `super_admin_group_users` (
   `SUPER_ADMIN_GROUP_USERS_ID` int(10) NOT NULL,
@@ -324,8 +319,6 @@ CREATE TABLE IF NOT EXISTS `super_admin_group_users` (
   KEY `fk_SUPER_ADMIN_GROUP_USERS_USERS1` (`USERS_ID`),
   CONSTRAINT `fk_SUPER_ADMIN_GROUP_USERS_USERS1` FOREIGN KEY (`USERS_ID`) REFERENCES `users` (`USERS_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Data exporting was unselected.
 
 
 -- Dumping structure for table smart_nehiborhood.trip_registry
@@ -346,8 +339,6 @@ CREATE TABLE IF NOT EXISTS `trip_registry` (
   CONSTRAINT `fk_TRIP_REGISTRY_RESIDENT_LOCATION1` FOREIGN KEY (`SOURCE_LOCATION_ID`) REFERENCES `resident_location` (`LOCATION_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_TRIP_REGISTRY_RESIDENT_LOCATION2` FOREIGN KEY (`DESTINATION_LOCATION_ID`) REFERENCES `resident_location` (`LOCATION_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Data exporting was unselected.
 
 
 -- Dumping structure for table smart_nehiborhood.trip_schedule
@@ -370,8 +361,6 @@ CREATE TABLE IF NOT EXISTS `trip_schedule` (
   CONSTRAINT `fk_TRIP_SCHEDULE_RESIDENT_LOCATION2` FOREIGN KEY (`DESTINATION_LOCATION_ID`) REFERENCES `resident_location` (`LOCATION_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-
 
 -- Dumping structure for table smart_nehiborhood.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -385,8 +374,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   CONSTRAINT `fk_USERS_USERS_STATUS_ID1` FOREIGN KEY (`USERS_STATUS_ID`) REFERENCES `users_status` (`USERS_STATUS_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-
 
 -- Dumping structure for table smart_nehiborhood.users_status
 CREATE TABLE IF NOT EXISTS `users_status` (
@@ -395,8 +382,9 @@ CREATE TABLE IF NOT EXISTS `users_status` (
   PRIMARY KEY (`USERS_STATUS_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-
+INSERT INTO `users_status` (`USERS_STATUS_ID`, `USERS_STATUS_NAME`) VALUES
+	(1, 'ACTIVE'),
+	(2, 'INACTIVE');
 
 -- Dumping structure for table smart_nehiborhood.user_resident_mapping
 CREATE TABLE IF NOT EXISTS `user_resident_mapping` (
@@ -411,8 +399,6 @@ CREATE TABLE IF NOT EXISTS `user_resident_mapping` (
   CONSTRAINT `fk_USER_RESIDENT_MAPPING_USERS2` FOREIGN KEY (`USERS_ID`) REFERENCES `users` (`USERS_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-
 
 -- Dumping structure for table smart_nehiborhood.vehicle_details
 CREATE TABLE IF NOT EXISTS `vehicle_details` (
@@ -420,13 +406,11 @@ CREATE TABLE IF NOT EXISTS `vehicle_details` (
   `VEHICLE_TYPE_ID` int(10) NOT NULL,
   `VEHICLE_REGISTRATION_NO` varchar(20) NOT NULL,
   `MAX_OCCUPANCY` int(3) NOT NULL,
+  `VEHICLE_NICK_NAME` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`VEHICLE_DETAILS_ID`),
   KEY `fk_VEHICLE_DETAILS_ID_VEHICLE_TYPE1` (`VEHICLE_TYPE_ID`),
   CONSTRAINT `fk_VEHICLE_DETAILS_ID_VEHICLE_TYPE1` FOREIGN KEY (`VEHICLE_TYPE_ID`) REFERENCES `vehicle_type` (`VEHICLE_TYPE_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Data exporting was unselected.
-
 
 -- Dumping structure for table smart_nehiborhood.vehicle_type
 CREATE TABLE IF NOT EXISTS `vehicle_type` (
@@ -435,7 +419,10 @@ CREATE TABLE IF NOT EXISTS `vehicle_type` (
   PRIMARY KEY (`VEHICLE_TYPE_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+CREATE TABLE IF NOT EXISTS `vendor_type` (
+  `VENDOR_TYPE_ID` int(10) NOT NULL,
+  `VENDOR_TYPE_NAME` varchar(50) NOT NULL,
+  `DEFAULT_TYPE` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`VENDOR_TYPE_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
